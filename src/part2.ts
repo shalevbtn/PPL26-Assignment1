e507de10a2349d71f89dedb7e02d054d118711b6
Nehoray Nitzan Biton <nehorayn@post.bgu.ac.il>
2026-03-30 13:57:59 +0300

    2.2

--- src/part2.ts
+++ src/part2.ts
@@ -7,7 +7,14 @@ const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
 export const countVowels: (s: string) => number = undefined as any;;
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
+export const isPalindrome = (text: string): boolean => {
+    const cleanText: string = text.toLowerCase();
+    const isAlphaNumeric: (char: string) => boolean = (char: string) => /[a-z0-9]/.test(char);
+    const chars: string[] = stringToArray(cleanText);
+    const fixedChars: string[] = R.filter(isAlphaNumeric, chars);
+    const reversedChars: string = R.reduceRight((acc:string, char: string) => char + acc, "", fixedChars)
+    return fixedChars.join('') === reversedChars;
+};
   
 
 /* Question 2.3 */
